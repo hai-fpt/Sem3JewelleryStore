27205fc06210fdcb55597e9cc20a5a4f4bd73d82
hainvh <hainvh@mz.co.kr>
2023-03-28 10:05:32 +0700

    fe

---
+++ jewellery-store-fe/src/components/Item/ItemList.jsx
@@ -0,0 +1,28 @@
+import Item from './Item';
+import getPageTitle from '../../helpers/getPageTitle';
+
+import Grid from '@mui/material/Grid';
+
+const ItemList = ({ items }) => {
+    // const [items]
+    return (
+        <>
+            <h2>{getPageTitle()}</h2>
+            <hr/>
+
+            <Grid container spacing={4} py={3}>
+                {/*{items?.map((item) => (*/}
+                {/*    <Grid item xs={12} sm={6} lg={3} key={item.id}>*/}
+                {/*        <Item {...item} />*/}
+                {/*    </Grid>*/}
+                {/*))}*/}
+                <Grid xs={12} sm={6} lg={3}>
+                    <Item {...items}/>
+                </Grid>
+                {/*{console.log(items)}*/}
+            </Grid>
+        </>
+    )
+};
+
+export default ItemList;
